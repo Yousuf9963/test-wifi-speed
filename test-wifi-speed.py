adcc421028f017c99abcb8f8fb8a5c5326488400
programmer0101 <113084773+Yousuf9963@users.noreply.github.com>
2022-12-09 06:03:25 +0300

    uploaded whoami.py

---
+++ test-wifi-speed.py
@@ -0,0 +1,18 @@
+print("[&] Coded By Yousuf Shafi'i Muhammad Junior Programmer")
+
+print("")
+
+print("[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog")
+
+
+print("")
+print("[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program")
+
+print("")
+print("I hope for you good future and i am willing that you will come high effort.")
+print("")
+import speedtest
+wifi = speedtest.Speedtest()
+print("Wifi Download Speed is ", wifi.download())
+print("")
+print("Wifi Upload Speed is ", wifi.upload())
